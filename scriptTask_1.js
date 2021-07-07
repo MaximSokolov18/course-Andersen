e94005ec7762a493e5429461b9bd41153b6d1ad6
trener-107 <makssokolov107@gmail.com>
2021-07-07 13:09:29 +0300

    fix: add check range secondNumber

--- scriptTask_1.js
+++ scriptTask_1.js
@@ -6,6 +6,9 @@ function isNumeric(str) {
 }
 
 if (isNumeric(firstNumber) && isNumeric(secondNumber)) {
+  while (+secondNumber < 2 || secondNumber > 36) {
+    secondNumber = prompt("Plese, enter the second number (form 2 to 36)", " ");
+  }
   console.log((+firstNumber).toString(+secondNumber));
 } else {
   console.log("Некорректный ввод!");
