7942e736d1d798cdb632c13347b56388dfd38a4b
trener-107 <makssokolov107@gmail.com>
2021-07-10 13:52:56 +0300

    result output

--- scriptLesson_2.js
+++ scriptLesson_2.js
@@ -1,1 +1,21 @@
-import { isEmpty, makePairs, isEqual } from './functionObj,js';
+import { isEmpty, makePairs, isEqual } from './functionObj.js';
+
+const data = {
+  a: null,
+  b: '',
+  c: undefined,
+};
+const data2 = {
+  a: 1,
+  b: 2,
+};
+const data3 = {
+  a: 1,
+  b: 2,
+};
+
+console.log(isEqual(data2, data3));
+console.log(isEqual(data2, data));
+console.log(makePairs(data));
+console.log(isEmpty(data));
+console.log(isEmpty(data2));
