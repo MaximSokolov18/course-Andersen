20c89d7dbcbd1e76e9818bce573343b4718b626f
trener-107 <makssokolov107@gmail.com>
2021-07-10 13:44:25 +0300

    feat: add function isEmpty()

--- functionObj.js
+++ functionObj.js
@@ -1,3 +1,25 @@
 export const isEqual = (firstObj, secondObj) => {
   return JSON.stringify(firstObj) === JSON.stringify(secondObj);
 };
+
+function isValid(value) {
+  if (!value && value !== 0) {
+    return false;
+  }
+
+  return true;
+}
+
+export const isEmpty = (object) => {
+  const arrayValues = Object.values(object);
+
+  if (arrayValues.length === 0) {
+    return true;
+  }
+
+  if (arrayValues.some((item) => isValid(item))) {
+    return false;
+  } else {
+    return true;
+  }
+};
