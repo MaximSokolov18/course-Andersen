99d8baf9793f8c55a620ca30511ed22a77ce083c
trener-107 <makssokolov107@gmail.com>
2021-07-13 11:40:18 +0300

    add method array myFilter()

--- scriptLesson_3.js
+++ scriptLesson_3.js
@@ -9,3 +9,13 @@ function getNumberVowels(str) {
   const vowels = ['a', 'u', 'i', 'o', 'e'];
   return str.split('').filter((litter) => vowels.includes(litter)).length;
 }
+
+Array.prototype.myFilter = function (callback, thisArg = this) {
+  let result = [];
+  for (let i = 0; i < thisArg.length; i++) {
+    if (callback(thisArg[i], i, thisArg)) {
+      result.push(thisArg[i]);
+    }
+  }
+  return result;
+};
