1737c36bfca491bef56a573eb6ebc697e038e102
trener-107 <makssokolov107@gmail.com>
2021-07-20 19:56:18 +0300

    add decision task-1

---
+++ scriptLesson_5.js
@@ -0,0 +1,56 @@
+//Оброботчики событий попадают по порядку в webAPI
+//После нажатия на кнопку callback попадает в очередь под №1
+button.addEventListener('click', () => {
+  //callback попадает в стек вызова
+  //Промис попадает в очередь как микрозадача
+  Promise.resolve().then(() => console.log('Microtask 1'));
+  //Выводиться "Listener 1"
+  console.log('Listener 1');
+});
+//После каждого выполнения макрозадач будет просматриваться очередь микрозадач
+//Event loop возьмёт задачу с очереди микрозадач так как макрозадача закончилась
+//Выпонялняеться микрозадача 'Microtask 1'
+//И только поcле этого пойдёт слудующий callback
+
+//callback попадает в очередь под №2
+button.addEventListener('click', () => {
+  //callback попадает в стек вызова
+  //Промис попадает в очередь как микрозадача
+  Promise.resolve().then(() => console.log('Microtask 2'));
+  //Выводиться "Listener 2"
+  console.log('Listener 2');
+});
+//После выпонялняеться микрозадача 'Microtask 2'
+
+// Результат:
+// 1)Listener 1
+// 2)Microtask 1
+// 3)Listener 2
+// 4)Microtask 2
+
+//Отправляется в webAPi
+button.addEventListener('click', () => {
+  Promise.resolve().then(() => console.log('Microtask 1'));
+  console.log('Listener 1');
+});
+
+//Отправляется в webAPi
+button.addEventListener('click', () => {
+  Promise.resolve().then(() => console.log('Microtask 2'));
+  console.log('Listener 2');
+});
+
+//Вызываеться click(),
+//после чего вызывает callback слушателей событий
+//то есть они добавляються в стек вызова
+//Дальше промисы попадают в очередь микрозадач по порядку,
+// а console.log() вызываеться по порядку
+// после опустошения стека выводяться микрозадачи
+button.click();
+// При нажатии на кнопку будет отрабатываться первый вариант
+
+// Результат:
+// 1)Listener 1
+// 2)Listener 2
+// 3)Microtask 1
+// 4)Microtask 2
