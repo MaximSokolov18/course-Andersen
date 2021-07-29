f8187638913edc3170795d645881e0048a5fa7c0
trener-107 <makssokolov107@gmail.com>
2021-07-29 14:54:31 +0300

    correction

--- scriptLesson_7.js
+++ scriptLesson_7.js
@@ -1,5 +1,6 @@
 const API_KEY = '5e4a694c6379ad3ea4353dcff926b2ae';
 const WEATHER_URL = 'https://api.openweathermap.org/';
+const NUMBER_DAY = 4;
 const dataCitiesArray = [
   {
     id: 710791,
@@ -28,7 +29,7 @@ function showDataWeather(weather) {
   console.log(
     `\u23A2 Date: ${date} \u23A2  \u23A2 Temp (max/min): ${Math.round(
       weather.temp.max
-    )}\u2103/${Math.round(weather.temp.min)} \u2103 \u23A2  \u23A2 Weather: ${
+    )}\u2103/${Math.round(weather.temp.min)}\u2103 \u23A2  \u23A2 Weather: ${
       weather.weather[0].description
     } \u23A2  \u23A2 Wind: ${weather.wind_speed}m/s \u23A2  \u23A2 Humidity: ${
       weather.humidity
@@ -38,14 +39,18 @@ function showDataWeather(weather) {
 
 function weatherRequest(dataCity) {
   fetch(
-    `${WEATHER_URL}data/2.5/onecall?lat=${dataCity.coord.lat}&lon=${dataCity.coord.lon}&exclude=hourly,minutely,alerts&units=metric&appid=${API_KEY}`
+    `${WEATHER_URL}data/2.5/onecall?lat=${dataCity.coord.lat}&lon=${dataCity.coord.lon}\
+&exclude=hourly,minutely,alerts\
+&units=metric\
+&appid=${API_KEY}`
   )
     .then((response) => response.json())
     .then((arrDataWeather) => {
       const weatherNow = arrDataWeather.current;
-      const NUMBER_DAY = 4;
       console.log(
-        `Country: ${dataCity.country}\nCity: ${dataCity.name}\nWeather now: ${weatherNow.temp}\u2103 ${weatherNow.weather[0].description}, wind speed ${weatherNow.wind_speed}m/s`
+        `Country: ${dataCity.country}\nCity: ${dataCity.name}\n\
+Weather now: ${weatherNow.temp}\u2103 ${weatherNow.weather[0].description},\
+ wind speed ${weatherNow.wind_speed}m/s`
       );
       arrDataWeather.daily
         .slice(0, NUMBER_DAY)
